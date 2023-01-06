a2b93a3c7fc2d9c9b247f55c3bbb6b181c2bbb7b
John Roberts <bigjlr2004@yahoo.com>
2023-01-06 14:50:08 -0600

    added weather pull down

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,14 +1,34 @@
 import { fetchWeather, getWeather } from "../data/dataAccess.js";
 
-export const showSelectedWeather = () => {
+export const showChrisWeather = () => {
   const weather = getWeather();
   return `
   ${weather
     .map((w) => {
-      const date = Date(w.dt_txt)
+      
       return `
-        The temperature on ${date} is: ${Math.round(w.main.temp)}\u00B0F`;
+        The temperature on ${w.dt_txt} is: ${Math.round(w.main.temp)}\u00B0F`;
     })
     .join("<br>")}
   `;
 };
+
+export const showSelectedWeather = () => {
+
+    const weather = getWeather()
+
+    return `
+    ${weather
+      .map((w) => {
+        
+        return `
+        <ul>
+        <li><img src="http://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png"></li>
+        <li>${w.dt_txt}</li>
+        <li>${w.weather[0].description}</li>
+        <li>temp: ${Math.round(w.main.temp)}\u00B0F</li></ul>`;
+      })
+      .join("<br>")}
+    `;
+    list[0].weather[0]
+}
