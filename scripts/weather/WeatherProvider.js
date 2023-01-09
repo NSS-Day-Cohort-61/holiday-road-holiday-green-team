d988f071bea96556b2f3974b9df0d307e85ddeaa
John Roberts <bigjlr2004@yahoo.com>
2023-01-09 07:45:06 -0600

    weather updated

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -19,22 +19,94 @@ export const showSelectedWeather = () => {
     return `
     ${weather
       .map((w) => {
+        const smonth = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
         const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
         const d = new Date(`${w.dt_txt}`)
-        let name = month[d.getMonth()];
-       
-        return `
         
-        <div class="date_container">
-        <ul class="dates">
-        <li>${name}, ${d.getDate()}</li>
-        <li><img src="http://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png"/></li>
-        <li>${w.weather[0].description}</li>
+        const sname = smonth[d.getMonth()];
+        const name = month[d.getMonth()];
+        if (d.getHours() === 3) {
+          return `
         
-        <li>${d.getHours()}:00</li>
-        <li>temp: ${Math.round(w.main.temp)}\u00B0F</li></ul></div>`;
-      })
+          <div class="date_container">
+              <ul class="dates">
+                <li>${name}, ${d.getDate()}</li>
+                <li>${d.getHours()}:00</li>
+                <li><img src="http://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png"/></li>
+                <li>${Math.round(w.main.temp)}\u00B0F</li>
+              </ul>
+          </div>`;
+        
+        } else if (d.getHours() === 6) {
+          return `
+        
+          <div class="date_container">
+              <ul class="dates">
+                <li>${sname}, ${d.getDate()}</li>
+                <li><img src="http://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png"/></li>
+                <li>${d.getHours()}:00</li>
+                <li>${Math.round(w.main.temp)}\u00B0F</li>
+              </ul>
+          </div>`;
+        
+        } else if (d.getHours() === 12) {
+          return `
+        
+          <div class="date_container">
+              <ul class="dates">
+                <li>${sname}, ${d.getDate()}</li>
+                <li class="big_temp">${Math.round(w.main.temp)}\u00B0F</li>
+                <li>${d.getHours()}:00</li>
+                <li>${w.weather[0].description}</li>
+              </ul>
+          </div>`;
+        
+        } else if (d.getHours() === 15) {
+          return `
+        
+          <div class="date_container">
+              <ul class="dates">
+                <li>${sname}, ${d.getDate()}</li>
+                <li class="big_temp">${Math.round(w.main.temp)}\u00B0F</li>
+                <li>${d.getHours()}:00</li>
+                <li>${w.weather[0].description}</li>
+              </ul>
+          </div>`;
+        
+        } else if (d.getHours() === 18) {
+          return `
+         
+          <div class="date_container" id="bck_image">
+              <ul class="dates">
+                <li>${sname}, ${d.getDate()} ${d.getHours()}:00</li>
+                <li class="a">____</li>
+                <li class="a">____</li>
+                <li class="big_temp">${Math.round(w.main.temp)}\u00B0F</li>
+                <li>${w.weather[0].description}</li>
+                
+              </ul>
+          </div>`;
+        
+        }
+        
+          else  {
+            return `
+          <div class="date_container">
+            <ul class="dates">
+              <li>${name}, ${d.getDate()}</li>
+              <li>${d.getHours()}:00</li>
+              <li><img src="http://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png"/></li>
+              <li>${w.weather[0].description}</li>
+              <li>${Math.round(w.main.temp)}\u00B0F</li>
+            </ul>
+          </div>`;
+        }
+
+        
+
+        
+  })
       .join("<br>")}
     `;
     
