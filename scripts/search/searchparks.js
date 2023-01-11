3e3f52f60e3c9891bfac1475f43f520f83a57f2d
John Roberts <bigjlr2004@yahoo.com>
2023-01-11 13:37:30 -0600

    searh form

--- scripts/search/searchparks.js
+++ scripts/search/searchparks.js
@@ -4,42 +4,29 @@ import { getParks } from "../data/dataAccess.js";
 
 
 export const parkActivities = () => {
-    const parks = getParks();
-    
-    const parkActivity = parks[0].activities.map((activity) => ({ ...activity }))
+
     let html = `
 
-    <div id="searchFunction">
-        <div class="search-wrapper">
-        <label for="search">Search</label>
-        </div>
-        <input type="search" id="search" data-search>
+    <div class="form-container">
+              <div>
+                  <input id="searchInput" type="text" class="input" placeholder="search..."/>
+                  <button id="search">Search</button>
+              </div>
+
+    </div>
+    <div class="results-container>
+       <ul class="results-list" id="list">
+
+       </ul>
     </div>
             
-        ${parkActivity
-          .map((activity) => {
-            return `
-            <div class="card">
-              <div class="header" data-header id="${activity.id}">${activity.name}</div>
-            </div>`
-        }).join("")}
-        </select>
-        `;
-    return html;
-     };
+          `
+          return html
+    }
 
-     document.addEventListener("input", (e) => {
-   
-        if (e.target === ["data-search"]) {
-            const searchTerm = e.target.elements["data-search"].values
-            const parks = getParks()
-            console.log(searchTerm)
-        //     const parkActivity = parks[0].activities.map((activity) => ({ ...activity }))
-        //     const value = e.target.value.toLowerCase()
-        //  parkActivity.forEach(activity =>{
-        //     const isVisible = 
-        //     activity.name.toLowerCase().includes(value)
-        //     document.getElementById([`${activity.id}`]).classList.toggle("hide",!isVisible)
-        //  })
+    document.addEventListener("click", (e) => {
+        if (e.target.id === "search") {
+          let value = document.getElementById("searchInput").value
+           console.log(value)
         }
-    })
+      });
